413d4886da151f77180ee0d8ecf2ef3578ff2902
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-05-25 22:43:38 -0500

    feat: Additional styles and finishing touches

--- README.md
+++ README.md
@@ -6,8 +6,4 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+This landing page project demonstrates how to generate navigation items from page content, and how to smoothly scroll to anchors on the page. Just open the index page and scroll or click around. Additionally, you can add new sections annotated with data-nav in the HTML to see the navigation generation
--- js/app.js
+++ js/app.js
@@ -26,21 +26,17 @@ let currentSection = sections[0];
 */
 
 const toggleActiveSection = (activeSection) => {
-  currentSection.classList.remove("your-active-class");
+  currentSection.classList.toggle("your-active-class");
   activeSection.classList.toggle("your-active-class");
   currentSection = activeSection;
 }
 
-const scrollTo = (selector) => {
+const scrollToSelector = (selector) => {
   document.querySelector(selector).scrollIntoView({
     behavior: 'smooth'
   });
 }
-
 // * End Helper Functions
-
-
-
 
 // * Begin Main Functions
 
@@ -50,22 +46,20 @@ const buildNavListItems = () => {
   const navbarUl = document.getElementById('navbar__list');
   const fragment = document.createDocumentFragment();
 
-  for (section of sections) {
-    item = document.createElement('li');
-    item.innerHTML = `<a href="#${section.id}">${section.dataset.nav} |</a>`;
+  for (const section of sections) {
+    const item = document.createElement('li');
+    item.innerHTML = `<a href="#${section.id}">${section.dataset.nav}&nbsp</a>`;
     fragment.appendChild(item);
   }
 
   navbarUl.appendChild(fragment);
 }
 
-buildNavListItems();
-
 // Add class 'active' to section when near top of viewport
 const findActive = () => {
   const lastSection = sections.length - 1;
   let pick = sections[lastSection];
-  for (section of sections) {
+  for (const section of sections) {
     const bounding = section.getBoundingClientRect();
     const offset = 75;
     if (bounding.bottom < offset) { continue; }
@@ -76,18 +70,14 @@ const findActive = () => {
   if (pick != currentSection) { toggleActiveSection(pick); }
 }
 
-document.addEventListener('scroll', findActive)
-
-toggleActiveSection(currentSection);
-
-// Scroll to anchor ID using scrollTO event
+// Scroll to anchor ID
 const navMenu = document.querySelector('.navbar__menu');
 navMenu.addEventListener('click', function(event) {
   event.preventDefault();
   if (event.target.nodeName === 'A') {
     const targetHref = event.target.href;
     const scrollTarget = targetHref.substring(targetHref.lastIndexOf("#"));
-    scrollTo(scrollTarget);
+    scrollToSelector(scrollTarget);
   }
 })
 
@@ -98,7 +88,8 @@ navMenu.addEventListener('click', function(event) {
 */
 
 // Build menu
-
+buildNavListItems();
 // Scroll to section on link click
-
+document.addEventListener('scroll', findActive)
 // Set sections as active
+toggleActiveSection(currentSection);
